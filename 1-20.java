626fd456063ecbb6641ff9177a1d54b63889a25e
jintaoguan <ilovexiao77@gmail.com>
2013-04-24 01:16:51 -0400

    problem 2,6

--- 1-20.java
+++ 1-20.java
@@ -3,7 +3,7 @@ public class Solution
 	public static void main( String[] argv )
 	{
 		Solution so = new Solution();
-		so.Problem_1();
+		so.Problem_6();
 	}
 	public void Problem_1()
 	{
@@ -11,8 +11,37 @@ public class Solution
 		for( int i = 0; i < 1000; ++i )
 		{
 			if( i % 3 == 0 || i % 5 == 0 )
+			{
 				sum += i;
+			}
 		}
 		System.out.println(sum);
+	}
+	public void Problem_2()
+	{
+		int sum = 0;
+		int[] dp = new int[40];
+		dp[0] = 0;
+		dp[1] = 1;
+		for( int i = 2; dp[i-1] <= 4000000; ++i )
+		{
+			dp[i] = dp[i-1] + dp[i-2];
+			if( dp[i] % 2 == 0 )
+				sum += dp[i];
+		}
+		System.out.println(sum);
+	}
+	public void Problem_6()
+	{
+		int diff = 0;
+		int sum1 = 0;
+		int sum2 = 0;
+		for( int i = 1; i <= 100; ++i )
+		{
+			sum1 += i * i;
+			sum2 += i;
+		}
+		diff = sum2 * sum2 - sum1;
+		System.out.println( diff );
 	}
 }
