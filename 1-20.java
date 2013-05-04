47f6252af09bf3d37dcc276291ff2d5fd56b24e3
jintaoguan <ilovexiao77@gmail.com>
2013-05-04 02:04:02 -0400

    solution to Problem 7

--- 1-20.java
+++ 1-20.java
@@ -3,7 +3,7 @@ public class Solution
 	public static void main( String[] argv )
 	{
 		Solution so = new Solution();
-		so.Problem_4();
+		so.Problem_7();
 	}
 	public void Problem_1()
 	{
@@ -65,27 +65,27 @@ public class Solution
 		long numm = 600851475143L;
 		long largestFact = 0;
 		long[] factors = new long[2];
-		 
+
 		for (long i = 2; i * i < numm; i++) 
 		{
 			if (numm % i == 0)
 			{
 				factors[0] = i;
 				factors[1] = numm / i;
-		 
-		        for (int k = 0; k < 2; k++) {
-		            boolean isPrime = true;
-		            for (long j = 2; j * j <  factors[k]; j++) {
-		                if (factors[k] % j == 0) {
-		                    isPrime = false;
-		                    break;
-		                 }
-		             }
-		             if (isPrime && factors[k] > largestFact) {
-		                largestFact = factors[k];
-		            }
-		        }
-		    }
+
+				for (int k = 0; k < 2; k++) {
+					boolean isPrime = true;
+					for (long j = 2; j * j <  factors[k]; j++) {
+						if (factors[k] % j == 0) {
+							isPrime = false;
+							break;
+						}
+					}
+					if (isPrime && factors[k] > largestFact) {
+						largestFact = factors[k];
+					}
+				}
+			}
 		}
 		System.out.println(largestFact);
 	}
@@ -115,5 +115,23 @@ public class Solution
 		}
 		if( rev == n ) return true;
 		return false;
+	}
+	public void Problem_7()
+	{
+		final int MAX = 1000000;
+		boolean[] prime = new boolean[MAX];
+		for( int i = 0; i < MAX; ++i )
+			prime[i] = true;
+		int cnt = 0;
+		int index = 2;
+		while( cnt != 10001 )
+		{
+			if( prime[index] == true )
+				cnt++;
+			for( int i = 2; index * i < MAX; ++i )
+				prime[ index * i ] = false;
+			index++;
+		}
+		System.out.println(index-1);
 	}
 }
