d54b3a5b0fce4d6324a4d9a0783493776223e111
jintaoguan <ilovexiao77@gmail.com>
2013-05-04 01:50:24 -0400

    solution to Problem 4

--- 1-20.java
+++ 1-20.java
@@ -3,7 +3,7 @@ public class Solution
 	public static void main( String[] argv )
 	{
 		Solution so = new Solution();
-		so.Problem_3();
+		so.Problem_4();
 	}
 	public void Problem_1()
 	{
@@ -65,27 +65,55 @@ public class Solution
 		long numm = 600851475143L;
 		long largestFact = 0;
 		long[] factors = new long[2];
+		 
 		for (long i = 2; i * i < numm; i++) 
 		{
 			if (numm % i == 0)
 			{
 				factors[0] = i;
 				factors[1] = numm / i;
-
-				for (int k = 0; k < 2; k++) {
-					boolean isPrime = true;
-					for (long j = 2; j * j <  factors[k]; j++) {
-						if (factors[k] % j == 0) {
-							isPrime = false;
-							break;
-						}
-					}
-					if (isPrime && factors[k] > largestFact) {
-						largestFact = factors[k];
-					}
-				}
-			}
+		 
+		        for (int k = 0; k < 2; k++) {
+		            boolean isPrime = true;
+		            for (long j = 2; j * j <  factors[k]; j++) {
+		                if (factors[k] % j == 0) {
+		                    isPrime = false;
+		                    break;
+		                 }
+		             }
+		             if (isPrime && factors[k] > largestFact) {
+		                largestFact = factors[k];
+		            }
+		        }
+		    }
 		}
 		System.out.println(largestFact);
+	}
+	public void Problem_4()
+	{
+		int max = Integer.MIN_VALUE;
+		for( int i = 999; i >= 100; --i )
+		{
+			for( int j = 999; j >= 100; --j )
+			{
+				int num = i * j ;
+				if( isPalindromic( num ) && ( num > max )  )
+					max = i * j;
+			}
+		}
+		System.out.println(max);
+	}
+	public boolean isPalindromic( int n )
+	{
+		int rev = 0;
+		int num = n;
+		while( num != 0 )
+		{
+			int digit = num % 10;
+			rev = rev * 10 + digit;
+			num = num / 10;
+		}
+		if( rev == n ) return true;
+		return false;
 	}
 }
