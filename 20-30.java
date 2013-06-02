fa84bbd92bf729c63fc4c688e4f46a0d6a4746f7
jintaoguan <ilovexiao77@gmail.com>
2013-06-01 20:42:42 -0400

    solution to Problem 21

--- 20-30.java
+++ 20-30.java
@@ -18,6 +18,25 @@ public class Solution
 		}
 		System.out.println(cnt);
 	}
+	public void Problem_21()
+	{
+		int sum = 0;
+		for( int i = 2; i <= 10000; ++i )
+		{
+			int sod = sumOfDivisor(i);
+			if( sod > i && sumOfDivisor(sod) == i) 
+				sum = sum + i + sod;
+		}
+		System.out.println(sum);
+	}
+	public int sumOfDivisor( int n )
+	{
+		int sum = 0;
+		for( int i = 1; i <= n/2 ; ++i )
+			if( n % i == 0 )
+				sum += i;
+		return sum;
+	}
 	public static void main( String[] argv )
 	{
 		Solution so = new Solution();
