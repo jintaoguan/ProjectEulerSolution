22d3e7bdfc157506c53697e34fe6e800d5e747c6
jintaoguan <ilovexiao77@gmail.com>
2013-06-02 01:36:58 -0400

    solution to Problem 22

--- 20-30.java
+++ 20-30.java
@@ -1,4 +1,9 @@
 import java.math.BigInteger;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Solution
 {
@@ -36,6 +41,41 @@ public class Solution
 			if( n % i == 0 )
 				sum += i;
 		return sum;
+	}
+	public void Problem_22()
+	{
+		String buffer = "";
+		try {
+			BufferedReader fr = new BufferedReader( new FileReader("/home/jintao/Desktop/names.txt") );
+			String line = fr.readLine();
+			buffer += line;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String format = buffer.replaceAll("\"", "");
+		String[] names = format.split(",");
+		Arrays.sort(names);
+		for( int i = 0; i < names.length; ++i )
+			System.out.println( names[i]);
+		System.out.println( names.length);
+		long sum = 0;
+		for( int i = 0; i < names.length; ++i )
+		{
+			int tmp = 0;
+			String name = names[i];
+			for( int j = 0; j < name.length(); ++j )
+			{
+				int index = name.charAt(j) - 'A';
+				tmp += (index+1);
+			}
+			System.out.println(tmp);
+			sum += tmp * (i+1);
+		}
+		System.out.println(sum);
 	}
 	public static void main( String[] argv )
 	{
